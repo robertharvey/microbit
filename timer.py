71d74ed5b5ce97d040fe8548fd7126a2f69bf9ab
Robert Harvey <rob@robharvey.co.uk>
2018-08-18 15:35:35 +0100

    Speed up display loop Ensure time doesn't get displayed twice

--- timer.py
+++ timer.py
@@ -4,7 +4,7 @@ from microbit import *
 
 MAX_TIME = 60
 
-ONE_SECOND = 1000
+UPDATE_TIME = 100
 FINAL_DISPLAY_TIME = 5*3*400
 
 STATE_STOPPED = 0
@@ -16,8 +16,9 @@ state = STATE_STOPPED
 old_state = STATE_STOPPED
 
 start_time = 0
-do_display = False
+last_time = 0
 clock = 0
+last_display_time = 0
 
 finished = [Image("00000:"
                   "00000:"
@@ -53,21 +54,21 @@ def show_dots(pos):
 display.show([Image.ARROW_W,' '], loop=True, wait=False)
 
 while True:
-
+    
     # Start timer
     if button_a.was_pressed():
         state = STATE_UPDATE
         start_time = last_time = running_time()
         clock = 0
     
-    # Reset
+    # Reset timer
     if button_b.was_pressed():
         state = STATE_STOPPED
         start_time = 0
         display.clear()
 
     # Update every second when running
-    if state == STATE_RUNNING and running_time() > last_time + ONE_SECOND:
+    if state == STATE_RUNNING and running_time() > last_time + UPDATE_TIME:
         last_time = running_time()
         state = STATE_UPDATE
     
@@ -77,18 +78,17 @@ while True:
         state = STATE_STOPPED
         
     if state == STATE_UPDATE:
-        time = int(MAX_TIME-(last_time-start_time)/1000)
-        if time <= 0:
+        display_time = int(MAX_TIME-(last_time-start_time)/1000)
+        if display_time <= 0:
             state = STATE_FINISHED
             display.show(finished, delay=400, wait=False, loop=True)
         else:
             state = STATE_RUNNING
-            if time > 10 and time % 10 == 0:
-                display.show(time)
-            elif time > 9:
+            if (display_time != last_display_time and display_time > 10 and display_time % 10 == 0) or display_time < 10:
+                display.show(display_time, wait=True)
+                last_display_time = display_time
+            else:
                 show_dots(clock)
                 clock = clock + 1
-            else:
-                display.show(str(time), wait=False)
         
     sleep(50)
